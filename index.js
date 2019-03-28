9d61b319c499c34cb2486ee3636fadf94bfcdd60
Antonio <antonio.giordano@getapper.com>
2019-03-28 15:21:04 +0100

    travis added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const input = require('./lib/input')
 process.GLOBAL = {}
 let env = process.env.NODE_ENV || 'production'
 if(env === 'test') {
-  console.log = function() {}
+  console.log = () => null
 }
 
 input()
